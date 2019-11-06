429467478c72742b595eccfedd2868b5fc1e4191
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 18:46:23 +0200

    Add files via upload

---
+++ No.7.py
@@ -0,0 +1,19 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=int(input("Enter integer. "))
+Enter integer. 24
+>>> if (x % 2) ==0:
+	print(x, "is even.")
+else:
+	print(x, "is Odd")
+
+	
+24 is even.
+>>> if (x % 4) ==0:
+	print(x, "is a multiple of 4")
+else:
+	print("It is not")
+
+	
+24 is a multiple of 4
+>>> 
