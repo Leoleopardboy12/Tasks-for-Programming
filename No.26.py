5f28545c51b626b908310c4ac8aa69671565fa9c
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 17:35:07 +0200

    Add files via upload

---
+++ No.26.py
@@ -0,0 +1,22 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> A= int(input())
+3
+>>> if A<=0:
+	A=0
+	print(A)
+else if 0<A<1:
+	
+SyntaxError: invalid syntax
+>>> if A<=0:
+	A=0
+	print(A)
+elif 0<A<1:
+	print(A)
+else:
+	F=A*4
+	print(F)
+
+	
+12
+>>> 
