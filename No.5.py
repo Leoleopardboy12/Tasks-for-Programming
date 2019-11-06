e00d4328de3c3ad5239c17f20ba9b9a1a92d460a
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 18:36:27 +0200

    Add files via upload

---
+++ No.5.py
@@ -0,0 +1,11 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> byte= int(input("How many bytes?"))
+How many bytes?23450
+>>> kb= byte/1024
+>>> print(kb,"kilobytes")
+22.900390625 kilobytes
+>>> mb=(byte/1024)/1024
+>>> print(mb,"megabytes")
+0.022363662719726562 megabytes
+>>> 
