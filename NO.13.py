de5c73366046f8c323254650b2c3245b5c89c7bd
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 19:27:25 +0200

    Add files via upload

---
+++ NO.13.py
@@ -0,0 +1,20 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> A=int(input("Width."))
+Width.22
+>>> D=int(input("DIameter. "))
+DIameter. 12
+>>> AreaA=A**2
+>>> print(AreaA)
+484
+>>> AreaC=3.14*(D**2)
+>>> print(AreaC)
+452.16
+>>> if AreaC>AreaA:
+	print("Possible.")
+else:
+	print("Not Possible.")
+
+	
+Not Possible.
+>>> 
