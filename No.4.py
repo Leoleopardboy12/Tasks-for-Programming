db6221cf8d0c34a435b9033e239cd0112d5f5066
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 18:31:13 +0200

    Add files via upload

---
+++ No.4.py
@@ -0,0 +1,11 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> w=int(input("Enter weight. "))
+Enter weight. 2340
+>>> kg = w/1000
+>>> print(kg,"kg")
+2.34 kg
+>>> ton=(w/1000)/1000
+>>> print(ton,"tons")
+0.00234 tons
+>>> 
