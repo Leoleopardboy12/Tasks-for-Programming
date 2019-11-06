20f599e2e7621ebb27ad6186547d82ff5cfcda53
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 18:41:14 +0200

    Add files via upload

---
+++ No.6.py
@@ -0,0 +1,10 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=int(input(">0"))
+>023
+>>> y=int(input(">0"))
+>012
+>>> z= 1/(x*y)
+>>> print(z)
+0.0036231884057971015
+>>> 
