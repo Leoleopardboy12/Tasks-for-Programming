de00027044b5532934f5c77b4ecf91b47043633b
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 18:07:46 +0200

    Add files via upload

---
+++ No.25.py
@@ -0,0 +1,14 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> A= int(input())
+1
+>>> if A<=2:
+	 F= (A*2)+(4*A)+5
+	 print(F)
+else:
+	F=1/((A*2)+(4*A)+5)
+	print(F)
+
+	
+11
+>>> 
