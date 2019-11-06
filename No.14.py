33e922c9c20746dbfd5714152a67789c2722e1a3
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 19:51:39 +0200

    Add files via upload

---
+++ No.14.py
@@ -0,0 +1,22 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> S= int(input())
+123
+>>> R= int(input("Radius. "))
+Radius. 23
+>>> import math
+>>> Side = math.sqrt(S)
+>>> print(Side)
+11.090536506409418
+>>> K=int(input())
+34
+>>> if (Side -(R*2))>=K;
+SyntaxError: invalid syntax
+>>> if (Side -(R*2))>=K:
+	print("Possible.")
+else:
+	print("Not Possible.")
+
+	
+Not Possible.
+>>> 
