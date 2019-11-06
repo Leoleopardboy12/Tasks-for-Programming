e9f9265ffccade0819401862ebda3961061e7436
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 18:27:11 +0200

    Add files via upload

---
+++ No.3.py
@@ -0,0 +1,16 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a= 33
+>>> b =22
+>>> c= 23
+>>> per =(a+b+c)/2
+>>> print(per)
+39.0
+>>> import math
+>>> x = per*(per-a)*(per-b)*(per-c)
+>>> print(x)
+63648.0
+>>> area = math.sqrt(x)
+>>> print(area)
+252.28555249954366
+>>> 
