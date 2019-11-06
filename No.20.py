1eb03973684e839205f7d0cc5095485fd68f76d3
Esenang <55867967+Leoleopardboy12@users.noreply.github.com>
2019-11-06 20:13:32 +0200

    Add files via upload

---
+++ No.20.py
@@ -0,0 +1,16 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = int(input())
+23
+>>> a = int(input())
+11
+>>> b=int(input())
+50
+>>> if a<=x and b>=x:
+	print(x, "is between", a, b)
+else:
+	print(x, "is NOT between", a, b)
+
+	
+23 is between 11 50
+>>> 
